ca00e0923d611723a8595ad8afbc2591c94812b2
mrkefir4ik <joker19980621@gmail.com>
2021-10-20 11:16:36 +0300

    feat: implemented main logig

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,77 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, screen } from '@testing-library/react';
+import QuestionList from './QuestionList';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+let result = null;
+
+beforeEach(() => {
+  result = render(<QuestionList />);
+});
+
+test('input is disabled if company is not chosen', () => {
+  const inputElement = result.container.querySelector('input');
+  expect(inputElement).toBeInTheDocument();
+  expect(inputElement).toBeDisabled();
+});
+
+test('input is enabled if company is chosen', () => {
+  const companyLogo = result.container.querySelector('img');
+
+  fireEvent(
+    companyLogo,
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+    }))
+
+  const inputElement = result.container.querySelector('input');
+  expect(inputElement).toBeInTheDocument();
+  expect(inputElement).toBeEnabled();
+});
+
+
+test('log after click on company logo', () => {
+
+  console.log = jest.fn();
+
+  const companyLogo = result.container.querySelector('img');
+
+  fireEvent(
+    companyLogo,
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+    }))
+
+  expect(console.log).toHaveBeenCalledWith('Selected Company')
+});
+
+test('log entered amount', () => {
+
+  console.log = jest.fn();
+
+  const inputElement = result.container.querySelector('input');
+
+  fireEvent.change(inputElement, { target: { value: '1000' } })
+
+  expect(console.log).toHaveBeenCalledWith('Entered Dollar Amount: $1000')
+});
+
+test('calculations are correct', async () => {
+
+  const companyLogo = result.container.querySelector('img');
+
+  fireEvent(
+    companyLogo,
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+    }))
+
+  const inputElement = result.container.querySelector('input');
+
+  fireEvent.change(inputElement, { target: { value: '1000' } })
+
+  expect(screen.getByText('$5,560')).toBeInTheDocument()
 });
--- src/QuestionList.tsx
+++ src/QuestionList.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import NumberFormat from 'react-number-format';
+import classNames from 'classnames';
 
-const tiers = [
+const tiers: { name: string, website: string, return: number, features: string[], }[] = [
   {
     name: 'Apple',
     website: 'apple.com',
@@ -13,7 +14,7 @@ const tiers = [
   {
     name: 'Tesla',
     website: 'tesla.com',
-    return:  2037,
+    return: 2037,
     features: [
       'Electric vehicles can be better, quicker and more fun to drive than gasoline cars.',
     ],
@@ -21,10 +22,26 @@ const tiers = [
 ]
 
 export default function QuestionList() {
-  
-  const initialAmount = 1000
-  const fiveYearReturn = 456
-  const investmentAfterFiveYears = fiveYearReturn * 0.01 * initialAmount + initialAmount;
+
+  const [value, setValue] = useState<string>('');
+
+  const [selectedCompany, setSelectedCompany] = useState<string>('');
+
+  const onLogoClick = (e: any) => {
+    setSelectedCompany(e.target.dataset.company);
+    console.log("Selected Company");
+    setValue('')
+  }
+
+  const onValueChange = (e: any) => {
+    setValue(e.target.value)
+    console.log(`Entered Dollar Amount: $${e.target.value}`);
+  }
+
+  const initialAmount: number = Number(value);
+  const fiveYearReturn: number | undefined = tiers.find(e => e.name === selectedCompany)?.return;
+  // @ts-ignore: Object is possibly 'undefined'.
+  const investmentAfterFiveYears: number | undefined = fiveYearReturn * 0.01 * initialAmount + initialAmount;
 
   return (
     <div className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
@@ -75,31 +92,39 @@ export default function QuestionList() {
         </svg>
         <div className="text-center">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Buying a new Apple or Tesla is exciting.
-</h2>
+          </h2>
           <p className="mt-4 text-lg leading-6 text-gray-500">
             However, investing can be fun too! </p>
           <p className="mt-4 text-lg leading-6 text-gray-500">See how rich you would be if you had bought the company's stock.
           </p>
         </div>
         {tiers.map((tier) => (
-                <div key={tier.name} className="flex flex-col overflow-hidden">
-                  <div className="px-6 py-8 bg-white sm:p-10 sm:pb-6">
-                    <div>
-                      <img className='object-center h-50 w-50' src={'//logo.clearbit.com/' + tier.website} alt={tier.name + ' logo'}/>
-                    </div>
-                  </div>
-                </div>
-              ))}
+          <div key={tier.name} className="flex flex-col overflow-hidden">
+            <div className="px-6 py-8 bg-white sm:p-10 sm:pb-6">
+              <div>
+                <img className={classNames('object-center h-50 w-50 focus:outline-black', { 'border-solid border-4 border-blue-500': selectedCompany === tier.name })} tabIndex={0} onKeyPress={(e) => e.key === 'Enter' && onLogoClick(e)} onClick={onLogoClick} data-company={tier.name} src={'//logo.clearbit.com/' + tier.website} alt={tier.name + ' logo'} />
+              </div>
+            </div>
+          </div>
+        ))}
         <div className="mt-12">
           <form action="#" method="POST" className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
-           
+
             <div className="sm:col-span-2">
               <label htmlFor="cost" className="block text-sm font-medium text-gray-700">
                 Amount of Money
               </label>
               <div className="mt-1">
                 <input
-                  type="text"
+                  disabled={!selectedCompany}
+                  onKeyPress={e => {
+                    if (e.key === '.') e.preventDefault();
+                  }}
+                  onChange={onValueChange}
+                  value={value}
+                  type="number"
+                  min='1'
+                  step='1'
                   name="cost"
                   id="cost"
                   className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
@@ -107,19 +132,19 @@ export default function QuestionList() {
               </div>
             </div>
           </form>
-          
+
         </div>
         {investmentAfterFiveYears > 1 &&
           <div className="mt-5">
             <p className="text-2xl text-center tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-4xl block xl:inline">
-            You would have <NumberFormat value={investmentAfterFiveYears} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={0} /> if you invested ${initialAmount} in Apple five years ago.
+              You would have <NumberFormat value={investmentAfterFiveYears} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={0} /> if you invested ${initialAmount} in {selectedCompany} five years ago.
             </p>
           </div>
         }
       </div>
       <div className="text-center">
-      <a href="https://clearbit.com">Logos provided by Clearbit</a>
-      <img src={'invite_education.png'} className="" alt="Invite Education Logo" />
+        <a href="https://clearbit.com">Logos provided by Clearbit</a>
+        <img src={'invite_education.png'} className="" alt="Invite Education Logo" />
       </div>
     </div>
   )
